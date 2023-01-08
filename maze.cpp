d4ba1aec76a967de0a0795381497425fe1c5c39e
_Imaginary_ <87122405+CHHHOCHHH@users.noreply.github.com>
2023-01-08 21:55:09 +0800

    Update maze.cpp

--- maze.cpp
+++ maze.cpp
@@ -1,85 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int mod=1000000007;
 const int dx[8]={-1,-1,-1,0,0,1,1,1},dy[8]={-1,0,1,-1,1,-1,0,1};
-int n;
+int hsh(int x,int y)
+{
+	return x*1000+y;
+}
+pair<int,int> unhsh(int x)
+{
+	return make_pair(x/1000,x%1000);
+}
 char s[505][505];
-char dir[2005];
-int cnt=0;
+int f[250005];
+int find(int x){return f[x]==x?x:f[x]=find(f[x]);}
+struct edge{
+	int u,v;
+}G[500005];
+void add(int x,int y)
+{
+	int x1=unhsh(x).first,x2=unhsh(y).first;
+	int y1=unhsh(x).second,y2=unhsh(y).second;
+	for(int i=2*x1;i<=2*x2;i++)
+		for(int j=2*y1;j<=2*y2;j++)
+			s[i][j]='A';
+}
+int m,n;
 int main()
 {
-	
 	printf("The size of your maze:\n");
 	scanf("%d",&n);
 	srand(time(0));
-	int S=rand()%(n-3)+2,T=rand()%(n-3)+2;
-	
-//	printf("S=%d T=%d\n",S,T);
-	
-	if(T>S) for(int i=T;i>S;i--) dir[++cnt]='D';
-    else for(int i=S;i>T;i--) dir[++cnt]='U';
-    for(int i=1;i<=n-1;i++) dir[++cnt]='R';
-    n=2*n-1;
+	int S=rand()%n+1,T=rand()%n+1;
+	S=2*S,T=2*T;
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n-1;j++)
+		{
+			G[++m]=edge{hsh(i,j),hsh(i,j+1)};
+			G[++m]=edge{hsh(j,i),hsh(j+1,i)};
+		}
+	random_shuffle(G+1,G+m+1);
+	random_shuffle(G+1,G+m+1);
+	random_shuffle(G+1,G+m+1);
+	for(int i=1;i<=1000*n+n;i++) f[i]=i;
+	for(int i=1;i<=m;i++)	
+	{
+		int u=G[i].u,v=G[i].v;
+		if(find(u)==find(v)) continue;
+		f[find(u)]=find(v);
+		add(u,v);
+	}
+	n=2*n+1;
 	for(int i=1;i<=n;i++) s[i][1]=s[i][n]=s[1][i]=s[n][i]='#';
-    int ta=rand()%(n/3)+1;
-    for(int i=1;i<=ta;i++) dir[++cnt]='L',dir[++cnt]='R';
-    ta=rand()%(n/3)+1;
-    for(int i=1;i<=ta;i++) dir[++cnt]='U',dir[++cnt]='D';
-    random_shuffle(dir+1,dir+cnt+1);
-    random_shuffle(dir+1,dir+cnt+1);
-    random_shuffle(dir+1,dir+cnt+1);
-    for(int i=1;i<cnt;i++)
-    {
-    	if(dir[i]+dir[i+1]=='L'+'R'||dir[i]+dir[i+1]=='U'+'D')
-    	{
-    		dir[i]=dir[i+1]='X';
-		}
-	}
-//    printf("%s\n",dir+1);
-	S=2*S-1,T=2*T-1;
-    int cx=S,cy=1;
-    int ax,ay;
-    int uu=0,dd=0,rr=0,LL=0;
-    while(dir[cnt]=='X') cnt--;
-    for(int i=1;i<=cnt;i++)
-    {
-    	ax=ay=0;
-    	switch(dir[i])
-    	{
-    		case 'R':if(LL) LL--; else ax=0,ay=1;break;
-    		case 'L':if(rr) rr--; else ax=0,ay=-1;break;
-    		case 'D':if(uu) uu--; else ax=1,ay=0; break;
-    		case 'U':if(dd) dd--; else ax=-1,ay=0;break;
-    		default:ax=ay=0;break;
-		}
-		if(!ax&&!ay) continue;
-		cx+=2*ax;
-		cy+=2*ay;
-		bool ok=1;
-		if(cx<1||cy<1||cx>n||cy>n)
-		{
-			switch(dir[i])
-	    	{
-	    		case 'R':ok=0,rr++;break;
-	    		case 'L':ok=0,LL++;break;
-	    		case 'D':ok=0,dd++; break;
-	    		case 'U':ok=0,uu++;break;
-	    		default:break;
-			}
-		}
-		cx-=2*ax;
-		cy-=2*ay;
-		if(ok)
-		{
-			s[cx][cy]='A';
-			cx+=ax;
-			cy+=ay;
-			s[cx][cy]='A';
-			cx+=ax;
-			cy+=ay;
-		}
-	}
 	for(int i=1;i<=n;i++)
 	{
 		for(int j=1;j<=n;j++)
